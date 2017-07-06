3ba370d7fae59a5097b90363d427c763fa085dd0
kiran <kirangarg700@gmail.com>
2017-07-06 23:26:44 +0530

    basic project layout initiated main.py and constants.py

---
+++ constants.py
@@ -0,0 +1,6 @@
+#all global variables is here
+
+#instagram api access token
+APP_ACCESS_TOKEN = '2008525763.b4eff92.5d4acde85c564413bce58eb73cc55d84'
+#instagram api base url
+BASE_URL = 'https://api.instagram.com/v1/'
