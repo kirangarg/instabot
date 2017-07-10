41bfe8ed52bd8164514c5e095b2950d2b0a4ef83
kiran <kirangarg700@gmail.com>
2017-07-10 20:57:01 +0530

    comment_user_post is modified

--- comment_user_post.py
+++ comment_user_post.py
@@ -1,7 +1,7 @@
 import requests
 from instabot import BASE_URL, APP_ACCESS_TOKEN
 from get_user_post import get_user_post
-username="vineet_chauhan9"
+#username="vineet_chauhan9"
 
 def comment_user_post(insta_username):
     media_id = get_user_post(insta_username)
@@ -9,6 +9,7 @@ def comment_user_post(insta_username):
     message = raw_input("Enter your Message: ")
     payload = {"access_token": APP_ACCESS_TOKEN, 'text': message}
     request_url = (BASE_URL + 'media/%s/comments') % (media_id)
+    print "POST request_url : %s" % (request_url)
     post_a_comment = requests.post(request_url, payload).json()
     print post_a_comment
     if post_a_comment['meta']['code'] == 200:
